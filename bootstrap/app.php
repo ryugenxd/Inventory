cea20fcc7d25a24c8fd0e3d34deaa27103f4587c
ryugenxd <anonimous7770@gmail.com>
2024-03-20 15:18:54 +0800

    update staging branch

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,11 +7,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+       $middleware -> alias([
+        'userlogin' => \App\Http\Middleware\CheckUserLogin::class,
+        'useractive' => \App\Http\Middleware\CheckUserActive::class,
+        'preventBackHistory' => \App\Http\Middleware\PreventBackHistory::class,
+        'checkRoleUser' => \App\Http\Middleware\CheckRoleUser::class,
+       ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
